529f630a5bb08b337010c4aeb613c2e428157637
Vadim <vpimeshkov@gmail.com>
2019-10-09 18:04:07 +0300

    Initial commit

---
+++ LimitedSizeStack.csproj/LimitedSizeStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        private readonly int Limit;
+        private readonly LinkedList<T> LinkedStack = new LinkedList<T>();
+
+        public LimitedSizeStack(int limit)
+        {
+            this.Limit = limit;
+        }
+
+        public void Push(T item)
+        {
+            LinkedStack.AddLast(item);
+            if (LinkedStack.Count > Limit)
+            {
+                LinkedStack.RemoveFirst();
+            }
+        }
+
+        public T Pop()
+        {
+            var value = LinkedStack.Last.Value;
+            LinkedStack.RemoveLast();
+            return value;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return LinkedStack.Count;
+            }
+        }
+    }
+}
---
+++ LimitedSizeStack.csproj/ListModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class ListModel<TItem>
+    {
+        public List<TItem> Items { get; }
+        public int Limit;
+        public LimitedSizeStack<Tuple<TItem, int, bool>> Events;
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            Events = new LimitedSizeStack<Tuple<TItem, int, bool>>(limit);
+            Limit = limit;
+        }
+
+        public void AddItem(TItem item)
+        {
+            Items.Add(item);
+            Events.Push(new Tuple<TItem, int, bool>(item, Items.Count - 1, true));
+        }
+
+        public void RemoveItem(int index)
+        {
+            Events.Push(new Tuple<TItem, int, bool>(Items[index], index, false));
+            Items.RemoveAt(index);
+        }
+
+        public bool CanUndo()
+        {
+            return Events.Count == 0 ? false : true;
+        }
+
+        public void Undo()
+        {
+            var value = Events.Pop();
+            if (value.Item3)
+                Items.RemoveAt(value.Item2);
+            else
+                Items.Insert(value.Item2, value.Item1);
+        }
+    }
+}
